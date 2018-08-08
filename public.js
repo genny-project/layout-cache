2d5901529965fa647da49266a07ec3bf34fcfaf1
matthaywardwebdesign <matt@matthayward.com.au>
2018-08-08 10:29:02 +1000

    Updated behaviour of public endpoint

--- public.js
+++ public.js
@@ -9,49 +9,70 @@ app.use( cors());
 /* Listen on port 2224 */
 app.listen( 2224, () => console.log( 'Layout cache public server listening on port 2224!' ));
 
-/* Serve up layouts from the local directory */
-app.use(( req, res ) => {
-  let basePath = '/tmp/layouts/public';
+/* Create a public endpoint */
+app.get( '/public', ( req, res ) => {
+  /* Depending whether or not a repo url was provided define the base path */
+  let basePath = '/tmp/layouts';
 
   if ( req.query.url ) {
     basePath = process.env.DEV
-      ? `/tmp/layouts/${req.query.url.split( '/' )[1].split( '.git' )[0]}/public`
-      : `/tmp/${req.query.url.split( '/' )[1].split( '.git' )[0]}/public`;
+      ? `/tmp/layouts/${req.query.url.split( '/' )[1].split( '.git' )[0]}`
+      : `/tmp/${req.query.url.split( '/' )[1].split( '.git' )[0]}`;
   }
 
-  /* Check whether the path is a folder */
-  try {
-    const isDir = fs.lstatSync( `${basePath}${req.path.split( '?' )[0]}` ).isDirectory();
-
-    if ( isDir ) {
-      /* Get all of the files in the directory */
-      fs.readdir( `${basePath}${req.path.split( '?' )[0]}`, ( err, files ) => {
-        res.json( files.map( f => ({
-          name: f,
-          download_url: `${req.protocol}://${req.get( 'host' )}${req.originalUrl.split( '?' )[0]}/${f}`,
-          path: `${req.originalUrl.split( '?' )[0]}/${f}`,
-          modified_date: fs.statSync( `${basePath}${req.originalUrl.split( '?' )[0]}/${f}` ).mtime
-        })));
+  /* Attempt to read the public routing file */
+  fs.readFile( `${basePath}/public/routing.public.json`, ( err, data ) => {
+    /* If an error occured then return it */
+    if ( err ) {
+      res.status( 500 );
+      res.json({
+        error: 'An unexpected error occured loading the public routing configuration for this repository.'
       });
-
       return;
-    } else {
-      /* Read the file */
-      fs.readFile( `${basePath}${req.path.split( '?' )[0]}`, ( err, result ) => {
-        if ( err || !result ) {
-          res.status( 404 );
-          res.json({ error: 'File / folder not found' });
-
-          return;
-        }
-
-        res.send( result );
-      });
     }
-  } catch ( e ) {
-    res.status( 404 );
-    res.json({ error: 'File / folder not found' });
 
-    return;
-  }
+    let parsed = null;
+
+    try {
+      /* Convert the data to a string and JSON parse it */
+      parsed = JSON.parse( data.toString());
+    } catch ( e ) {
+      console.error( e );
+
+      res.status( 500 );
+      res.json({
+        error: 'An unexpected error occured loading the public routing configuration for this repository.'
+      });
+      return;
+    }
+
+    /* Loop over each file in the parsed file */
+    const files = parsed.map( entry => readJSONFile( `${basePath}/public/${entry.path}` ));
+
+    Promise.all( files ).then( result => {
+      res.json( parsed.map(( entry, index ) => {
+        entry.data = result[index];
+        return entry;
+      }));
+    });
+  });
 });
+
+/* Returns a promise that resolves when the file is loaded */
+function readJSONFile( path ) {
+  return new Promise(( resolve, reject ) => {
+    /* Read the file */
+    fs.readFile( path, ( err, data ) => {
+      if ( err ) {
+        return reject( err );
+      }
+
+      try {
+        const parsed = JSON.parse( data.toString());
+        return resolve( parsed );
+      } catch ( e ) {
+        return reject( e );
+      }
+    });
+  });
+}
