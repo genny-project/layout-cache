a091cd03ff826c287790456bc51e271df4418d6a
matthaywardwebdesign <matt@matthayward.com.au>
2018-08-13 15:09:27 +1000

    Altered sync behaviour

--- index.js
+++ index.js
@@ -117,39 +117,41 @@ app.use(( req, res ) => {
   /* Fetch the repo if needed */
   fetch( 'https://github.com/genny-project/layouts.git', () => {
     fetch( req.query.url, () => {
-      /* Check whether the path is a folder */
-      try {
-        const isDir = fs.lstatSync( `${basePath}${req.path.split( '?' )[0]}` ).isDirectory();
-        if ( isDir ) {
-          /* Get all of the files in the directory */
-          fs.readdir( `${basePath}${req.path.split( '?' )[0]}`, ( err, files ) => {
-            res.json( files.map( f => ({
-              name: f,
-              download_url: `${req.protocol}://${req.get( 'host' )}${req.originalUrl.split( '?' )[0]}/${f}`,
-              path: `${req.originalUrl.split( '?' )[0]}/${f}`,
-              modified_date: fs.statSync( `${basePath}${req.originalUrl.split( '?' )[0]}/${f}` ).mtime
-            })));
-          });
-          return;
-        } else {
-          /* Read the file */
-          fs.readFile( `${basePath}${req.path.split( '?' )[0]}`, ( err, result ) => {
-            if ( err || !result ) {
-              res.status( 404 );
-              res.json({ error: 'File / folder not found' });
-              return;
-            }
 
-            res.send( result );
-          });
-        }
-      } catch ( e ) {
-        res.status( 404 );
-        res.json({ error: 'File / folder not found' });
-        return;
-      }
     });
   });
+
+        /* Check whether the path is a folder */
+        try {
+          const isDir = fs.lstatSync( `${basePath}${req.path.split( '?' )[0]}` ).isDirectory();
+          if ( isDir ) {
+            /* Get all of the files in the directory */
+            fs.readdir( `${basePath}${req.path.split( '?' )[0]}`, ( err, files ) => {
+              res.json( files.map( f => ({
+                name: f,
+                download_url: `${req.protocol}://${req.get( 'host' )}${req.originalUrl.split( '?' )[0]}/${f}`,
+                path: `${req.originalUrl.split( '?' )[0]}/${f}`,
+                modified_date: fs.statSync( `${basePath}${req.originalUrl.split( '?' )[0]}/${f}` ).mtime
+              })));
+            });
+            return;
+          } else {
+            /* Read the file */
+            fs.readFile( `${basePath}${req.path.split( '?' )[0]}`, ( err, result ) => {
+              if ( err || !result ) {
+                res.status( 404 );
+                res.json({ error: 'File / folder not found' });
+                return;
+              }
+  
+              res.send( result );
+            });
+          }
+        } catch ( e ) {
+          res.status( 404 );
+          res.json({ error: 'File / folder not found' });
+          return;
+        }
 });
 
 /* Listen on port 2223 */
